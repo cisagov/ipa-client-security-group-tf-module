bed3d45a27b229285f19d04760c494094316309a
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-07-12 14:09:56 -0400

    Alphabetize variables

--- variables.tf
+++ variables.tf
@@ -4,13 +4,13 @@
 # You must provide a value for each of these parameters.
 # ------------------------------------------------------------------------------
 
-variable "vpc_id" {
-  description = "The ID of the AWS VPC where the security group will live (e.g. vpc-0123456789abcdef0)"
-}
-
 variable "ipa_server_cidr_blocks" {
   type        = list(string)
   description = "A list of CIDR blocks containing the IPA servers to which the clients will be allowed to connect (e.g. [100.10.1.23/32, 101.45.32.89/32])"
+}
+
+variable "vpc_id" {
+  description = "The ID of the AWS VPC where the security group will live (e.g. vpc-0123456789abcdef0)"
 }
 
 # ------------------------------------------------------------------------------
