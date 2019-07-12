ee31713623841e00502bf4df8ce9be1f098661da
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-07-12 14:22:21 -0400

    Alphabetize outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "id" {
-  value       = aws_security_group.ipa_clients.id
-  description = "The security group ID"
-}
-
 output "arn" {
   value       = aws_security_group.ipa_clients.arn
   description = "The security group ARN"
+}
+
+output "id" {
+  value       = aws_security_group.ipa_clients.id
+  description = "The security group ID"
 }
